46b9c9eec158f45965a7fb2508f061850a010176
Lucas F. Sil <109614514+lucasFelixSilveira@users.noreply.github.com>
2022-12-22 23:27:17 -0300

    2.4.3

---
+++ FIreray/README.md
@@ -0,0 +1,125 @@
+# FIreray  [ 2.4.3 ]
+
+<div align="center">
+    <img src="https://media.discordapp.net/attachments/983446685327966269/1041340936367644783/FIreray-removebg-preview.png?width=662&height=241" height="130">
+    <br>
+    Usar Array na Realtime da Firebase
+</div>
+
+# 💬 Linguagem
+> **click [here](https://github.com/lucasFelixSilveira/FIreray/blob/main/FIreray/assets/ENGLISH.md) to go to the documentation in English.**
+
+# ⁉ Sobre
+A FIreray trabalha com [Node.js](https://nodejs.org), em conjunto com a [Firebase](https://firebase.google.com/), fazendo a [Real Time](https://firebase.google.com/docs/database/web/start) aceitar arrays.
+
+- Sempre com novidades em produção
+- Frequentemente atualizada
+- O mais optmizada possível
+
+# 🟣 Instalação
+
+Abra seu projeto no Visual Studio ou outra IDE, abra o terminal e use: 
+```sh-session
+npm i fireray
+```
+- Aguarde a instação do modulo e pronto! Agora você já pode usar a **Firebase** com **Arrays**
+
+# ♻ Uso
+
+- `1.` Dependendo se você vai usar a **_[Fsdb](https://fsdb.tk)_** ou a **_[Firebase](https://firebase.google.com/)_** ( Não ambas ) você pode simplificar o uso da **FIreray** usando:
+```js
+FIreray.singleUse('fsdb')    
+```
+
+<div align="center"> ou <br><br> </div>
+
+```js
+FIreray.singleUse('firebase')    
+```
+- - O uso também é simples. Só se basear no modo de uso normal e usar função coletada.
+```js
+FIreray.use('get', async (func) => {
+    const get = await func(db, 'directory')
+    console.log(get)
+})
+```
+
+- `2.` Se você vai usar os 2, infelizmente você tera que sempre ficar usando o prefixo `fs` ou `fb`. Veja o exemplo abaixo:
+- - Lembrando que `fs` é para a Fsdb e `fb` para a Firebase!
+```js
+const db = fsdb.database();
+
+await FIreray.fsSet(db, 'users', [ { user: 0, name: 'Lucas F. Sil' }, { user: 1, name: 'Ana C. Rod' }, { user: 2, name: 'Vitor G. Sil' }, { user: 3, name: 'Kauã J. San' } ])
+const get = await FIreray.get(db, 'users');
+
+console.log(get)
+```
+
+# 🔗 Direções
+- [Módulo npm](https://www.npmjs.com/package/fireray)
+- [Projeto na github](https://github.com/lucasFelixSilveira/FIreray)
+- [Licença MIT](https://github.com/lucasFelixSilveira/FIreray/blob/main/FIreray/licence)
+- [Discord da comunidade](https://discord.gg/cdEnEtwehC)
+
+# 📄 Notas da versão
+- **2.4.1** & **2.4.2**
+```
+Correção de bugs
+```
+- **2.4.0**
+```
+Mudança do get para sistema simplificado
+```
+- **2.3.7**
+```
+Adição do "usage" no comando remove!
+```
+- **2.3.6**
+```
+Correção do bug do 0 no remove!
+Observação: https://www.youtube.com/watch?v=9URV25JlVK0
+```
+- **2.3.5**
+```
+Documentação em Inglês.
+```
+- **2.3.2** & **2.3.3**
+```
+Correção de bugs
+```
+- **2.3.1**
+```
+Adição do comando REMOVE
+- Remove itens indesejados de um array.
+```
+- **2.3.0**
+```
+Adição do sistema de alerta de atualizações
+- se comunica com a CLI requerindo a versão atual e verificando se é a em execução.
+```
+- **2.2.4**
+```
+Correção de bugs
+```
+- **2.2.3**
+```
+Adição de verificações TypeOf 
+( verificações de string para apenas aceitar STRINGS )
+( verificações de objects para apenas aceitar VARIAVEIS / OBJETOS )
+
+ps. No push e no Set não foram adicionados pois dentro de um array pode conter VÁRIOS tipos de dados.
+```
+- **2.2.1** & **2.2.2**
+```
+Correção de bugs
+```
+- **2.2.0**
+```
+Adição o sistema de notas de versão no Github e no Npm;
+Adição do comando REPLACE
+```
+
+# Notas do desenvolvedor
+
+Deseja ajudar no desenvolvimento?
+- Donate por Pix: `lucasdwbfff@gmail.com`
---
+++ FIreray/lib/response.js
@@ -0,0 +1,207 @@
+const get = require("fsdb.js/database/get")
+
+module.exports.run = (db, req) => retorno(db, req)
+
+function retorno(db, req) {
+    var dir = `arrays/${req}`
+    function fbGet() {
+        return new Promise((resolve, reject) => {
+            try {
+                db.ref(dir).once('value').then( async (x) => { 
+                    if( x.val() == null ) return resolve(undefined)
+                    let array; eval( `array = ${x.val().array}`); return resolve(array)
+                })
+            } catch(er) {
+                reject(er)
+                throw new TypeError('Firebase error.\n'+er)
+            }
+        })
+    }
+    function __get(varName, functionName) {
+        return `db.ref(\`${dir}\`).once('value').then((x) => { ${varName} = x.val()? eval(x.val().array):undefined; ${functionName}() })`
+    }
+    function fbSet(res) {
+        return new Promise((resolve, reject) => {
+            let array_ = `${JSON.stringify(res)}`;
+            if( res == null ) array_ = null
+            try {
+                db.ref(dir).set({ array: array_ })
+                resolve(200)
+            } catch(er) {
+                reject(er)
+                throw new TypeError(`Firebase error.\n`+er)
+            }
+        })
+    }
+    function fbPush(res) {
+        return new Promise(async (resolve) => {
+            const get = await fbGet();
+            let array;
+            if( get == undefined ) array = res
+            if( get !== undefined ) array = get;
+            if( get !== undefined ) array.push(res)
+            const set = await fbSet(array);
+            resolve(set)
+        })
+    }
+    function fbClone(res, forceSet) {
+        return new Promise((resolve, reject) => {
+            fbGet().then(x => {
+                dir = 'arrays/'+res
+                fsGet().then(x2 => {
+                    if( x2 == undefined ) fbSet(x).then(x => resolve(x));
+                    else {
+                        if(! forceSet ) throw TypeError('The Clone method cannot be used with the SUBMISSION directory being with some saved content.')
+                        else if( forceSet ) fbSet(x).then(x => resolve(x)) 
+                    }
+                })
+        })
+        })
+    }
+    function fbReplace(res) {
+        return new Promise(async (resolve) => {
+            resolve(await fsClone(res, true))
+        })
+    }
+    function fbRemove(res) {
+        return new Promise((resolve, reject) => {
+            fbGet().then(array2 => {
+                let array = array2;
+                if( array == undefined ) return; 
+                else {
+                    const arraySize = ( array.length -  1 );
+                    if( arraySize < res ) return;
+                    else {
+                        if( arraySize >= res ) {
+                            const newArray = [];
+                            array.forEach(
+                                function (item, index) {
+                                    if( index !== res ) newArray.push(item)
+                                    if( index == arraySize ) fbSet(newArray).then(x => resolve(x))
+                                }
+                            )
+                        } else return;
+                    }
+                }
+            })
+        })
+    }
+
+    const obj = new Object();
+    obj.fbGet = () => fbGet(); // V n° 1.0.0  -=/=- Manutenção na V n° 2.4.0
+    obj.fbSet = (res) => fbSet(res) // V n° 1.0.0
+    obj.fbPush = (res) => fbPush(res) // V n° 1.1.0
+    obj.fbClone = (res) => fbClone(res) // V n° 2.1.0
+    obj.fbReplace = (res) => fbReplace(res) // V n° 2.2.0
+    obj.fbRemove = (res) => fbRemove(res) // V n° 2.3.1
+
+
+    function fsGet() {
+        return new Promise((resolve, reject) => {
+            try {
+                db.get(dir).then(async (x) => { 
+                    if( x == null ) return resolve(undefined)
+                    const generateArray = () => {
+                        return new Promise((resolve, reject) => {
+                            let array_;
+                            resolve(eval( `array_ = ${x.array}`))
+                        })
+                    }
+                    let array = await generateArray(); 
+                    return resolve(array)
+                })
+            } catch(er) {
+                return reject(undefined)
+            }
+        })
+    }
+    function fsSet(res) {
+        return new Promise((resolve, reject) => {
+            let array_ = `${JSON.stringify(res)}`;
+            if( res == null ) array_ = null
+            try {
+                db.block(dir).set({ array: array_ }).then(resolve(200))
+            } catch(er) {
+                reject(er)
+            }
+        })
+    }
+    function fsDelete() {
+        return new Promise((resolve, reject) => {
+            try {
+                db.block(dir).delete().then((status) => {
+                    resolve(200)
+                });
+            } catch (er) {
+                reject(er)
+            }
+        })
+    }
+    function fsPush(res) {
+        new Promise(resolve => {
+            fsGet().then(x => {
+                const x2 = x ? x : []
+                x2.push(res)
+                fsSet(x2).then(y => {
+                    resolve(y)
+                })
+            })
+        })
+    }
+    function fsClone(res, forceSet) {
+        return new Promise(resolve => {
+            fsGet().then(x => {
+                dir = 'arrays/'+res
+                fsGet().then(x2 => {
+                    if( x2 == undefined ) fsSet(x).then(x => resolve(x));
+                    else {
+                        if(! forceSet ) throw TypeError('The Clone method cannot be used with the SUBMISSION directory being with some saved content.')
+                        else if( forceSet ) fsSet(x).then(x => resolve(x)) 
+                    }
+                })
+            })
+        })
+    }
+    function fsReplace(res) {
+        return new Promise(async (resolve) => { 
+            resolve(await fsClone(res, true))
+        })
+    }
+    function fsRemove(res) {
+        return new Promise((resolve, reject) => {
+            fsGet().then(array2 => {
+                let array = array2;
+                if( array == undefined ) return; 
+                else {
+                    const arraySize = ( array.length -  1 );
+                    if( arraySize < res ) return;
+                    else {
+                        if( arraySize >= res ) {
+                            const newArray = [];
+                            array.forEach(
+                                function (item, index) {
+                                    if( index !== res ) newArray.push(item)
+                                    if( index == arraySize ) fsSet(newArray).then(x => resolve(x))
+                                }
+                            )
+                        } else return;
+                    }
+                }
+            })
+        })
+    }
+
+    obj.fsGet = () => fsGet()
+    obj.fsSet = (res) => fsSet(res)
+    obj.fsDelete = () => fsDelete()
+    obj.fsPush = (res) => fsPush(res)
+    obj.fsClone = (res) => fsClone(res, false)
+    obj.fsReplace = (res) => fsReplace(res)
+    obj.fsRemove = (res) => fsRemove(res)
+
+    return obj;
+}
+
+/** 
+ * ≫ ≫ ≫ feito por Lucas Felix Silveira
+*/
